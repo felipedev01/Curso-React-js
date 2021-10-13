215a8b0b3fd1abc82de8dc9bea3f72bff436b79b
Felipe <felipecss88@gmail.com>
2021-10-13 19:50:10 -0300

    Ciclo de vida com UseEffect

--- src/header.js
+++ src/header.js
@@ -1,11 +1,29 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import { useState } from 'react'
+
 
 function Counter (props){
 
   const[count, setCount]=useState(props.count)
+
+
+  useEffect(()=>{
+
+    setCount(parseInt(localStorage.getItem("count")))
+
+  },[])
+
+  useEffect(()=>{
+
+  localStorage.setItem("count",count)
+
+
+  },[count])
+
+  
+  
   function add(){
 
      setCount(count+1)
